5589729cbd6877173083d634bbbb9591e098634b
Joseph Mate <mate.joseph@gmail.com>
2021-02-27 18:43:51 -0500

    make sure i don't accidentally mess up the large randomly generated file
    they take a long time to make

--- performance_testing/generate_files.ps1
+++ performance_testing/generate_files.ps1
@@ -7,7 +7,13 @@ cd ..\performance_testing
 #     10,000,000B           =    10 MB
 #    100,000,000B           =   100 MB
 #  1,000,000,000B           = 1,000 MB
-foreach ($num_chars in 1000000, 10000000, 100000000) {
-  $num_chars
-  ..\generate_large_files\target\release\generate_large_files.exe --characters $num_chars --repeated_text_length 100 --times 1000 --out tmp\large_file.$num_chars.txt --repeated_text_file tmp\repeated.$num_chars.txt
+foreach ($num_chars in 1000000, 10000000, 100000000, 1000000000) {
+  $large_file="tmp\large_file.$num_chars.txt"
+  $repeated_file="tmp\repeated.$num_chars.txt"
+  if (Test-Path $large_file -PathType leaf) {
+    Write-Output "$large_file already exists. please delete it and run again"
+  } else {
+    Write-Output "Generated $num_chars file $large_file"
+    ..\generate_large_files\target\release\generate_large_files.exe --characters $num_chars --repeated_text_length 100 --times 1000 --out $large_file --repeated_text_file $repeated_file
+  }
 }
